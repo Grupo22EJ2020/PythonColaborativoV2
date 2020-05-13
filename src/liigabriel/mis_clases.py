eb8b0d347d4eb51d657f97e038089560b7f65052
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-13 17:17:51 -0500

    add imprimirinfo metodo

--- src/liigabriel/mis_clases.py
+++ src/liigabriel/mis_clases.py
@@ -2,4 +2,6 @@ class Cargador():
     def __init__(self, color, marca):
         self.color = color
         self.marca = marca
-        
+    
+    def imprimirinfo(self):
+        print(f"Cargador \n Color: {self.color} \n Marca: {self.marca}")
