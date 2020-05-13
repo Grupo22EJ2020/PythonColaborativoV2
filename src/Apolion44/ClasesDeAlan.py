8754d2d8eed7cd389cca758a4b22f2293db4c031
Carlos Alan Guerrero Belmonte <jupiterguerrero92@gmail.com>
2020-05-13 17:33:50 -0500

    se agrego la clase y prueba de apolion

---
+++ src/Apolion44/ClasesDeAlan.py
@@ -0,0 +1,9 @@
+class Galleta (): 
+    def __init__(self,nombre,sabor): 
+        self.nombre = nombre 
+        self.sabor = sabor
+    def ImpresionDeGalleta (self): 
+        print (f"Tu galleta Favorita es{self.nombre} su sabor es{self.sabor}")
+
+
+
---
+++ src/Apolion44/PruebaDeCLaseDeAlan.py
@@ -0,0 +1,7 @@
+from ClasesDeAlan import Galleta 
+nombreDeGalleta = input("dime como se llama tu galleta favorita?")
+saborGalleta = input("de que sabor es? ")
+
+muestraGalleta = Galleta(nombreDeGalleta,saborGalleta)
+mostrar= muestraGalleta.ImpresionDeGalleta()
+
