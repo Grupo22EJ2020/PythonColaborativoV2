0c32f80c6fe4e3dae27f2df26019a6990f2bfa3e
Ximena Martitínez Hernández <ximenamthdz@gmail.com>
2020-05-15 14:23:51 -0500

    Agregue mis clases

---
+++ src/ximenahdz/Misclases.py
@@ -0,0 +1,21 @@
+class Blusa ():
+    def __init__ (self,talla,color,):
+        self.__talla=talla
+        self.__color=color
+
+    def Imprimirinfo (self):
+        print (f"talla:{self.__talla}")
+        print (f"color:{self.__color}")
+
+class Bolsa ():
+    def __init__ (self,tamaño,color,marca):
+        self.__tamaño=tamaño
+        self.__color=color
+        self.__marca=marca
+
+    def Caracteristicas (self):
+        print (f"tamaño:{self.__tamaño}")
+        print (f"color:{self.__color}")
+        print (f"marca:{self.__marca}")
+        
+
