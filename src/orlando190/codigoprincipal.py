9cec2b6389b7541f16e59b1ec489395f7f5f73b0
Orlando Mendoza Campos <orlando.mendozacm@uanl.edu.mx>
2020-05-18 13:33:09 -0500

    Se agregaron dos clases

--- src/orlando190/codigoprincipal.py
+++ src/orlando190/codigoprincipal.py
@@ -2,6 +2,26 @@ class PC:
     def __init__(self, procesador, ram):
         self.procesador = procesador
         self.ram = ram
-    def imprimirInfo(self):
+    def imprimirpc(self):
         print(f"PC \n procesador: {self.procesador} \n ram: {self.ram}")
+
+
+class Nieve:
+    def __init__(self, sabor, envase, precio):
+        self.sabor = sabor
+        self.envase = envase
+        self.precio = precio
+    def imprimirNIEVE(self):
+        print(f"Nieve \n sabor: {self.sabor} \n envase: {self.envase} \n precio: {self.precio}")
+
+
+class Teclado:
+    def __init__(self, precio, marca, modelo):
+        self.precio = precio
+        self.marca = marca
+        self.modelo = modelo
+    def imprimirTECLADO(self):
+        print(f"Teclado \n precio: {self.precio} \n marca: {self.marca} \n modelo: {self.modelo}")
+    
+    
 
--- src/orlando190/codigoprueba.py
+++
@@ -1,6 +0,0 @@
-from codigoprincipal import PC
-procesador = input("¿Que procesador tiene tu PC?: ")
-ram = input("¿Cuanta ram tiene tu PC?: ")
-
-mostrarcomponentes= PC(procesador,ram)
-mostrar2= mostrarcomponentes.imprimirInfo()
