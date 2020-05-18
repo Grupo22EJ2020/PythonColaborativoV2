2799a79d999779c6b4a87605645df89ad1a77098
RicardoBanda <alexjeje453@gmail.com>
2020-05-17 23:22:29 -0500

    Commit Inicial

---
+++ src/RicardoBanda/clasesme.py
@@ -0,0 +1,12 @@
+class Carro():
+    def __init__(self, num1, num2):
+        self.__num1=num1
+        self.__num2=num2
+    def suma(self):
+        return f"El resultado de la suma es {self.__num1+self.__num2}"
+        
+
+        
+        
+
+
