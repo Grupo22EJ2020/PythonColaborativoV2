310727b7b4d89596cdee13851645c576cb5d54ce
Jairo Manuel Batrez Gonzalez <jairo_real11@hotmail.com>
2020-05-22 19:05:41 -0500

    Agregue clase guitarra

--- src/jairobtz/jairoclases.py
+++ src/jairobtz/jairoclases.py
@@ -4,3 +4,14 @@ class Consola():
         self.marca = marca
      def imprimirConsola(self):
          print(f"Consola \n procesador: {self.procesador} \n marca: {self.marca}")
+
+
+
+class Guitarra():
+    def __init__(self,tipo,cuerdas,precio):
+        self.tipo = tipo
+        self.cuerdas = cuerdas
+        self.precio = precio
+     def imprimirGuitarra(self):
+         print(f"Guitarra \n tipo: {self.tipo} \n cuerdas: {self.cuerdas} \n precio: {self.precio}")
+
