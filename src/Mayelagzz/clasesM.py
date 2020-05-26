71558361f7927053ca0d06bee79e680e510e50b4
Mayela Gonzalez Martinez <mayelagzz99@gmail.com>
2020-05-26 17:29:54 -0500

    Add metodo imprimirinfo

--- src/Mayelagzz/clasesM.py
+++ src/Mayelagzz/clasesM.py
@@ -2,3 +2,6 @@ class Tenis():
     def __init__ (self, color, marca):
         self.color = color
         self.marca = marca
+
+def imprimirinfo(self):
+        print(f"Tenis \n Color: {self.color} \n Marca: {self.marca}")
