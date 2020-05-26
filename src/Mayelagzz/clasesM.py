ccb5e5ab9f98c71e12fda0f106971dffebb44ef6
Mayela Gonzalez Martinez <mayelagzz99@gmail.com>
2020-05-26 17:00:38 -0500

    Agrugue claseTacones

---
+++ src/Mayelagzz/clasesM.py
@@ -0,0 +1,19 @@
+class Tenis():
+    def __init__ (self, color, marca):
+        self.color = color
+        self.marca = marca
+    
+    def imprimirinfo(self):
+        print(f"Tenis \n Color: {self.color} \n Marca: {self.marca}")
+
+class Tacones():
+    def __init__ (self, color, marca):
+        self.color = color
+        self.marca = marca
+    
+    def imprimirinfo(self):
+        print(f"Tacones \n Color: {self.color} \n Marca: {self.marca}")
+
+
+
+    
