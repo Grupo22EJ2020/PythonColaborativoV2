4e5ba04f0bc98ff4fc34c6b606568bc3a10db1c2
Mayela Gonzalez Martinez <mayelagzz99@gmail.com>
2020-05-26 17:31:26 -0500

    Add las 3 clases

--- src/Mayelagzz/clasesM.py
+++ src/Mayelagzz/clasesM.py
@@ -5,3 +5,19 @@ class Tenis():
 
 def imprimirinfo(self):
         print(f"Tenis \n Color: {self.color} \n Marca: {self.marca}")
+
+class Tacones():
+    def __init__ (self, color, marca):
+        self.color = color
+        self.marca = marca
+    
+    def imprimirinfo(self):
+        print(f"Tacones \n Color: {self.color} \n Marca: {self.marca}")
+
+class Zapatos():
+    def __init__ (self, color, marca):
+        self.color = color
+        self.marca = marca
+    
+    def imprimirinfo(self):
+        print(f"Zapatos \n Color: {self.color} \n Marca: {self.marca}")
