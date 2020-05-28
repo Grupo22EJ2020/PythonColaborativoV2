402845b7fee6f037e592c17572cac0f94e809dbc
Oscar Ulises Caballero Mata <cmu33723@gmail.com>
2020-05-27 23:54:48 -0500

    Agregue mis clases oucm2410

---
+++ src/oucm2410/MisClases.py
@@ -0,0 +1,15 @@
+class Cargador():
+    def __init__(self, color, marca):
+        self.color = color
+        self.marca = marca
+    
+    def imprimirinfo(self):
+        print(f"Cargador \n Color: {self.color} \n Marca: {self.marca}")
+
+class Puerta():
+    def __init__(self, ancho, alto|):
+        self.ancho = ancho
+        self.alto = alto
+    
+    def imprimirinfo(self):
+        print(f"Puerta \n Ancho: {self.ancho} \n Alto: {self.alto}")
